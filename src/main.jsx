b93ba604f835af5810cdb071eebeedcee58a0e3d
alexanderperezscott <Alexanderperezscott@gmail.com>
2026-07-28 23:24:24 -0600

    fix main jsx double import

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
-
-// src/main.jsx (o src/index.jsx)
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import App from './App';
-import './index.css';
-
-// Reemplaza esto con tu Client ID real de Google Cloud Console
-const GOOGLE_CLIENT_ID = "TU_CLIENT_ID_DE_GOOGLE.apps.googleusercontent.com";
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
-  </React.StrictMode>
-);
